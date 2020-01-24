e2ce5c1d8849ae68637e9521818bc49aa6967f5d
Hazel Trinity <hazelrella11@gmail.com>
2020-01-23 19:18:05 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyglitch',
+    description='"Glitchy" image effects for Python',
+    author='Hazel Rella',
+    author_email='hazelrella11@gmail.com',
+    version='0.0.1',
+    packages=find_packages(include=['pyglitch', 'pyglitch.*']),
+    install_requires=[
+        'numpy==1.18.1',
+        'PIL==1.1.6'
+    ]
+)
