45d0a4919da173a6a2bd3f2bb79c8686d37b469f
Hazel Trinity <hazelrella11@gmail.com>
2020-01-23 18:49:04 -0800

    Added Bonsai Example

---
+++ examples/bonsai/exBonsai.py
@@ -0,0 +1,30 @@
+# Set working directory
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__)))))
+
+# bonsai
+
+from PIL import Image as _Image
+import pyglitch
+import logging
+
+intervalGenerator0 = pyglitch.PixelFunctionIntervalGenerator(
+    pyglitch.HuePixelFunction(),
+)
+
+logging.getLogger().setLevel(7)
+
+intervalGenerator0.optimizeImage(_Image.open('bonsai.jpg'), 333/360, goal=0.5)
+
+sortingFunction0 = pyglitch.LightnessPixelFunction()
+
+sortFilter0 = pyglitch.PixelSortFilter(
+    intervalGenerator0,
+    sortingFunction0,
+    axis=1
+)
+
+sortFilter0.apply(_Image.open('bonsai.jpg'), angle=45).save('bonsai_0.png')
